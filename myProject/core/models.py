504ea0d270be5394f4ba818432df5bca4389433a
Md Golam Maula <maula03@gmail.com>
2025-12-07 12:43:15 +0600

    crud operation class-1 done

---
+++ myProject/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    student_id = models.IntegerField()
+    full_name = models.TextField(max_length=150)
+    department = models.TextField(max_length=100)
+    dob = models.DateField()
+    gender = models.TextField(max_length=20)
+    address = models.TextField(max_length=200)
+    
+    def __str__(self):
+        
+        return f"{self.student_id} - {self.full_name}"
